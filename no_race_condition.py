04a2472f155c0784edae0ce80ab7c0c033ddc40b
Alexis Gommet <sherman503@hotmail.fr>
2022-05-18 19:44:03 +0200

    Initial commit

---
+++ no_race_condition.py
@@ -0,0 +1,28 @@
+from threading import Thread
+from time import sleep
+
+
+counter = 0
+
+def increase(by):
+    global counter
+
+    sleep(0.1)
+    
+    counter += by
+    
+    print(f'counter={counter}')
+
+# create threads
+t1 = Thread(target=increase, args=(10,))
+t2 = Thread(target=increase, args=(20,))
+
+# start the threads
+t1.start()
+t2.start()
+
+# wait for the threads to complete
+t1.join()
+t2.join()
+
+print(f'The final counter is {counter}')
